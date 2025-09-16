1379ef3675e510a35520486bbf1871395e03014c
Patrice Davis <pdavisiaa@gmail.com>
2025-09-15 21:18:41 -0500

    Update README, add visuals, and clean notebook

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,10 +1,11 @@
 import pandas as pd 
 from sqlalchemy import create_engine 
 
+#Connect to database
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/music_analytics")
+
 #Load CSV into pandas 
 df = pd.read_csv(r"C:\Users\Patrice Davis\Desktop\Projects\Music_Streaming_Analytics\data\sample_dataset.csv")
 
-# Connect to PostgreSQL
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/music_analytics")
-
+#Creates a table called 'tracks' if it doesn’t already exist, and loads the dataset into it
 df.to_sql("tracks", engine, if_exists="replace", index=False)
